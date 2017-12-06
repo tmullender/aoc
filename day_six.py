c5ae00ef64a38b7f12676f413db2eb843e4b6dd6
Tim Mullender <t.j.mullender@gmail.com>
2017-12-06 12:53:04 +0000

    D6P2

--- day_six.py
+++ day_six.py
@@ -4,23 +4,34 @@ import sys
 def redistribute(filepath):
     with open(filepath, 'r') as reader:
         banks = map(int, reader.readline().split())
-        history = {",".join(map(str, banks))}
+        history = {create_key(banks)}
         cycle = 0
+        last_seen = 0
         while True:
-            highest = max(banks)
-            bank = min([bank for bank, value in enumerate(banks) if value == highest])
+            bank = min([bank for bank, value in enumerate(banks) if value == max(banks)])
             value = banks[bank]
             cycle += 1
             banks[bank] = 0
-            while value > 0:
-                bank = (bank + 1) % len(banks)
-                banks[bank] += 1
-                value -= 1
-            key = ",".join(map(str, banks))
-            if key in history:
-                return cycle
+            distribute(bank, banks, value)
+            key = create_key(banks)
+            if key in history and last_seen > 0:
+                return cycle - last_seen
+            elif key in history:
+                last_seen = cycle
+                history = {key}
             history.add(key)
     return 0
+
+
+def create_key(banks):
+    return ",".join(map(str, banks))
+
+
+def distribute(bank, banks, value):
+    while value > 0:
+        bank = (bank + 1) % len(banks)
+        banks[bank] += 1
+        value -= 1
 
 
 if __name__ == "__main__":
