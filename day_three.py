0a4a76056e2088e8621cd3b37d0cca74d2ac937c
Tim Mullender <t.j.mullender@gmail.com>
2017-12-06 11:26:03 +0000

    D3P2

--- day_three.py
+++ day_three.py
@@ -1,14 +1,41 @@
 import sys
 
 
-def count_steps(target):
+def find_position(target):
     size = 1
-    while (size ** 2) <= target:
+    position = (0, 0)
+    history = {position: 1}
+    value = 0
+    while value < target:
         size += 2
-    x_offset = int(size/2)
-    y_offset = ((size**2 - target) % (size-1)) - size/2
-    return x_offset + abs(y_offset)
+        while position[1] < size/2:
+            position = (position[0], position[1]+1)
+            value = calculate_value(history, position)
+            if value > target:
+                return value
+        while position[0] > -size/2 + 1:
+            position = (position[0]-1, position[1])
+            value = calculate_value(history, position)
+            if value > target:
+                return value
+        while position[1] > -size/2 + 1:
+            position = (position[0], position[1]-1)
+            value = calculate_value(history, position)
+            if value > target:
+                return value
+        while position[0] < size/2:
+            position = (position[0]+1, position[1])
+            value = calculate_value(history, position)
+            if value > target:
+                return value
+    return value
+
+
+def calculate_value(history, position):
+    total = sum([v for k, v in history.iteritems() if abs(k[0] - position[0]) < 2 and abs(k[1] - position[1]) < 2])
+    history[position] = total
+    return total
 
 
 if __name__ == "__main__":
-    print count_steps(int(sys.argv[1]))
+    print find_position(int(sys.argv[1]))
