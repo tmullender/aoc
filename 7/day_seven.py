62db9073b5f9529d653b4c59bb000e5d4bcdd111
Tim Mullender <t.j.mullender@gmail.com>
2017-12-07 16:18:49 +0000

    D7P2

--- 7/day_seven.py
+++ 7/day_seven.py
@@ -5,20 +5,59 @@ import re
 PATTERN = re.compile("([a-z]+) \(([0-9]+)\)( -> (.*))?")
 
 
+def correct_weight(weights, programs):
+    if len(weights) == 1:
+        return 0
+    else:
+        invalid, initial, correct = 0, 0, 0
+        for k, v in weights.iteritems():
+            if len(v) == 1:
+                invalid = k
+                initial = programs[v[0]][0]
+            else:
+                correct = k
+        return initial + correct - invalid
+
+
+
+def sum_weights(start, programs):
+    details = programs[start]
+    total = details[0]
+    supported = details[1]
+    corrected = 0
+    if supported:
+        weights = {}
+        for program in supported:
+            weight, offset = sum_weights(program, programs)
+            if corrected == 0:
+                corrected = offset
+            total += weight
+            if weight in weights:
+                weights[weight].append(program)
+            else:
+                weights[weight] = [program]
+        if corrected == 0:
+            corrected = correct_weight(weights, programs)
+    return total, corrected
+
+
 def build_tower(file_path):
     programs = {}
     uppers = set()
     with open(file_path, 'r') as reader:
         for line in reader.readlines():
             details = PATTERN.search(line).groups()
-            programs[details[0]] = details
             supporting = details[3]
             if supporting:
                 uppers.update(supporting.split(", "))
+                programs[details[0]] = (int(details[1]), supporting.split(", "))
+            else:
+                programs[details[0]] = (int(details[1]), supporting)
+        start = None
         for program in programs:
             if program not in uppers:
-                return program
-    return None
+                start = program
+    return sum_weights(start, programs)
 
 
 if __name__ == "__main__":
