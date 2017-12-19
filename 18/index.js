5da8a4bd73db434b64673aa906a9b6f6594eee19
Tim Mullender <t.j.mullender@gmail.com>
2017-12-19 12:51:41 +0000

    DIP2

--- 18/index.js
+++ 18/index.js
@@ -1,8 +1,65 @@
 var exports = module.exports = {};
 
-function parseField(registers, input) {
-  var result = registers[input]
-  if (result){
+function Program(id, instructions) {
+  this.id = id
+  this.instructions = instructions
+  this.queue = []
+  this.registers = {"p":id}
+  this.running = true
+  this.sent = 0
+}
+
+Program.prototype.run = function(i, other) {
+  this.running = true
+  while (this.running) {
+    const instruction = this.instructions[i].split(" ")
+    const update = this.process(instruction, other)
+    i += update
+    this.running = (update != 0 && 0 <= i && i < this.instructions.length)
+  }
+  return i
+}
+
+Program.prototype.process = function (instruction, other) {
+  const register = instruction[1]
+  switch (instruction[0]) {
+    case "set":
+      this.registers[register] = this.parseField(instruction[2])
+      break
+    case "add":
+      this.registers[register] += this.parseField(instruction[2])
+      break
+    case "mul":
+      this.registers[register] *= this.parseField(instruction[2])
+      break;
+    case "mod":
+      this.registers[register] %= this.parseField(instruction[2])
+      break;
+    case "snd":
+      other.queue.unshift(this.parseField(register))
+      other.running = true
+      this.sent += 1
+      break;
+    case "rcv":
+      var update = this.queue.pop();
+      if (typeof update != 'undefined') {
+        this.registers[register] = update;
+      } else {
+        return 0
+      }
+      break;
+    case "jgz":
+      if (this.parseField(register) > 0) {
+        return this.parseField(instruction[2])
+      }
+      break;
+  }
+  return 1
+}
+
+Program.prototype.parseField = function (input) {
+  var result = this.registers[input]
+  if (typeof result != 'undefined'){
     return result
   }
   return parseInt(input)
@@ -10,38 +67,12 @@ function parseField(registers, input) {
 
 exports.play = function (content) {
   const instructions = content.split("\n")
-  const registers = []
-  var lastPlayed = -1
-  var i = 0
-  while (0 <= i && i < instructions.length) {
-    const instruction = instructions[i].split(" ")
-    const register = instruction[1]
-    if (instruction[0] == "rcv" && registers[register] > 0) {
-      break
-    }
-    switch (instruction[0]) {
-      case "set":
-        registers[register] = parseField(registers, instruction[2])
-        break
-      case "add":
-        registers[register] += parseField(registers, instruction[2])
-        break
-      case "mul":
-        registers[register] *= parseField(registers, instruction[2])
-        break;
-      case "mod":
-        registers[register] %= parseField(registers, instruction[2])
-        break;
-      case "snd":
-        lastPlayed = registers[register]
-        break;
-      case "jgz":
-        if (registers[register] > 0) {
-          i += parseInt(instruction[2]) - 1
-        }
-        break;
-    }
-    i += 1
+  const zero = new Program(0, instructions)
+  const one = new Program(1, instructions)
+  var io = 0, iz = 0
+  while (one.running || zero.running) {
+    io = zero.run(io, one)
+    iz = one.run(iz, zero)
   }
-  return lastPlayed
+  return one.sent
 }
--- 18/test/test.js
+++ 18/test/test.js
@@ -6,7 +6,7 @@ describe('Examples', function() {
   describe('Sounds', function() {
     it('should return 4', function(done) {
       fs.readFile('day_eighteen.example', {encoding :'utf8' }, (_, content) => {
-        assert.equal(main.play(content), 4);
+        assert.equal(main.play(content), 3);
         done();
       });
     });
@@ -17,7 +17,7 @@ describe('Input', function() {
   describe('Sounds', function() {
     it('should return ?', function(done) {
       fs.readFile('day_eighteen.input', {encoding :'utf8' }, (_, content) => {
-        assert.equal(main.play(content), 4601);
+        assert.equal(main.play(content), 6858);
         done();
       });
     });
