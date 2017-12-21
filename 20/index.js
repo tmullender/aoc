937ffaebb9f2d7a2fb011a39b762fd80925f2319
Tim Mullender <t.j.mullender@gmail.com>
2017-12-21 00:20:11 +0000

    DKP2

--- 20/index.js
+++ 20/index.js
@@ -1,3 +1,4 @@
+var _ = require('lodash')
 var exports = module.exports = {};
 
 const pattern = /p=<([0-9-]+),([0-9-]+),([0-9-]+)>, v=<([0-9-]+),([0-9-]+),([0-9-]+)>, a=<([0-9-]+),([0-9-]+),([0-9-]+)>/
@@ -5,23 +6,45 @@ const pattern = /p=<([0-9-]+),([0-9-]+),([0-9-]+)>, v=<([0-9-]+),([0-9-]+),([0-9
 function convert(line) {
   const result = pattern.exec(line)
   const parseField = (x) => parseInt(x)
-  return {"p": result.slice(1, 4).map(parseField),
-    "v":result.slice(4,7).map(parseField),
-    "a":result.slice(7,10).map(parseField)}
+  return {position: result.slice(1, 4).map(parseField),
+    velocity:result.slice(4,7).map(parseField),
+    acceleration:result.slice(7,10).map(parseField)}
+}
+
+function move(particle){
+  const a = particle.acceleration
+  const v = particle.velocity
+  const p = particle.position
+  for (i=0;i<3;i++){
+    v[i] += a[i]
+    p[i] += v[i]
+  }
+}
+
+function positionSort(a, b){
+  var result = a.position[0] - b.position[0]
+  if (result != 0) {
+    return result
+  }
+  result = a.position[1] - b.position[1]
+  if (result != 0) {
+    return result
+  }
+  return a.position[2]-b.position[2]
+}
+
+function removeCollisions(particles) {
+  const positions = Object.values(_.groupBy(particles, 'position'))
+  return positions.filter((v) => v.length == 1).reduce((a,b) => a.concat(b))
 }
 
 exports.play = function (content) {
   const lines = content.split("\n").filter((x) => x.length > 0)
-  const particles = lines.map(convert)
-  var min = Number.POSITIVE_INFINITY
-  var particle = particles.length + 1
-  for (i=0; i < particles.length; i++){
-    const a = particles[i]["a"]
-    const total = Math.abs(a[0]) + Math.abs(a[1]) + Math.abs(a[2])
-    if (total < min) {
-      particle = i
-      min = total
-    }
+  var particles = lines.map(convert)
+  for (j=0;j<40 && particles.length > 1;j++) {
+    particles.forEach(move)
+    particles.sort(positionSort)
+    particles = removeCollisions(particles)
   }
-  return particle
+  return particles.length
 }
--- 20/test/test.js
+++ 20/test/test.js
@@ -4,9 +4,9 @@ const main = require('../index.js')
 
 describe('Particles', function() {
   describe('Examples', function() {
-    it('should return 0', function(done) {
+    it('should return 1', function(done) {
       fs.readFile('day_twenty.example', {encoding :'utf8' }, (_, content) => {
-        assert.equal(main.play(content), 0);
+        assert.equal(main.play(content), 1);
         done();
       });
     });
@@ -14,7 +14,7 @@ describe('Particles', function() {
   describe('Input', function() {
     it('should return ?', function(done) {
       fs.readFile('day_twenty.input', {encoding :'utf8' }, (_, content) => {
-        assert.equal(main.play(content), 119);
+        assert.equal(main.play(content), 471);
         done();
       });
     });
