76d82fe717276ce1304d4ed06633633bbc31c710
Tim Mullender <t.j.mullender@gmail.com>
2018-12-08 14:20:28 +0000

    Day 05 Part Two

--- 05/main.py
+++ 05/main.py
@@ -1,13 +1,23 @@
 #! /bin/env python
 
+
+def react(input):
+    i = 2
+    while i < len(input):
+        if input[i] != input[i - 1] and input[i].lower() == input[i - 1].lower():
+            input = input[0:i - 1] + input[i + 1:]
+            i -= 2
+        i += 1
+    return len(input) - 1
+
+
 if __name__ == "__main__":
     with open("input.txt", 'r') as reader:
-        line = reader.readline()
-        i = 1
-        while i < len(line):
-            if line[i] != line[i-1] and line[i].lower() == line[i-1].lower():
-                line = line[0:i-1] + line[i+1:]
-                i -= 2
-            i += 1
-        print len(line)
-        print line
+        line = "#" + reader.readline()
+        shortest = len(line)
+        for char in "abcdefghijklmnopqrstuvwxyz":
+            updated = line.replace(char, "").replace(char.upper(), "")
+            result = react(updated)
+            if result < shortest:
+                shortest = result
+        print shortest
