6161d096e8bf5c40b3bdec41cce3624df4d463fe
Tim Mullender <t.j.mullender@gmail.com>
2018-12-09 13:51:04 +0000

    Day 06 Part Two

--- 06/main.py
+++ 06/main.py
@@ -1,48 +1,15 @@
 #! /bin/env python
 
-import sys
-
-KEY = "abcedfeghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-
-def closest(i, j, options):
-    distance = 2000
-    location = None
+def in_range(i, j, options):
+    distance = 0
     for option in options:
         d = abs(i - option[0]) + abs(j-option[1])
-        if d == distance:
-            location = None
-        elif d < distance:
-            distance = d
-            location = options.index(option)
-    return (i, j), location
+        distance += d
+    return distance < 10000
 
 
-def collect(locations, l):
-    key = l[1]
-    if key in locations:
-        locations[key].append(l[0])
-    else:
-        locations[key] = [l[0]]
-    return locations
-
-
-def finite(locations, i, low, high):
-    return len(filter(lambda x: x[i] in (low, high), locations)) == 0
-
-
-def print_grid(input):
-    last = 0
-    for x in input:
-        if x[0][1] != last:
-            sys.stdout.write('\n')
-        if x[1] or x[1] == 0:
-            sys.stdout.write(KEY[x[1]])
-        else:
-            sys.stdout.write('.')
-        last = x[0][1]
-    sys.stdout.write('\n')
-    sys.stdout.flush()
+def collect(total, included):
+    return total + 1 if included else total
 
 
 if __name__ == "__main__":
@@ -56,13 +23,5 @@ if __name__ == "__main__":
         high_x = by_x[-1][0] + 1
         low_y = by_y[0][1]
         high_y = by_y[-1][1] + 1
-        grid = [closest(x, y, coords) for y in (range(low_y, high_y)) for x in (range(low_x, high_x))]
-        # print_grid(grid)
-        r = reduce(collect, grid, dict())
-        size = 0
-        biggest = None
-        for k in filter(lambda x: x and finite(r[x], 0, low_x, high_x-1) and finite(r[x], 1, low_y, high_y-1), r):
-            if len(r[k]) > size:
-                size = len(r[k])
-                biggest = k
-        print biggest, KEY[biggest], size, r[biggest]
+        grid = [in_range(x, y, coords) for y in (range(low_y, high_y)) for x in (range(low_x, high_x))]
+        print reduce(collect, grid, 0)
