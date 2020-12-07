4b6afefdc5df47ab506786d22bc5aed6adeffef8
Tim Mullender <t.j.mullender@gmail.com>
2020-12-07 22:36:47 +0000

    Day Seven - Part Two

--- 2020/src/commands/seven.ts
+++ 2020/src/commands/seven.ts
@@ -1,18 +1,29 @@
 import {Command} from '@oclif/command'
 import {readFileSync} from 'fs'
 
-function findOptions(x: string, tree: Map<string, Array<string>>, acc: Set<string>): Set<string> {
-  const options = tree.get(x)
-  options?.map(acc.add, acc)
-  options?.map(x => findOptions(x, tree, acc))
-  return acc
+class Rule {
+  count: number;
+
+  type: string;
+
+  constructor(desc: string) {
+    const match = /(\d*) ?(\w+ \w+) bag/.exec(desc)
+    this.count = match && match[1] ? parseInt(match[1], 10) : 0
+    this.type = match ? match[2] : 'unknown'
+  }
 }
 
-function extractTypes(desc: string): string[] {
-  return desc.split(',').map(y => {
-    const match = /\d* ?(\w+ \w+) bag/.exec(y)
-    return match ? match[1] : 'unknown'
+function countBags(x: string, tree: Map<string, Array<Rule>>): number {
+  const options = tree.get(x)
+  let count = 0
+  options?.forEach(r => {
+    count += (r.count * countBags(r.type, tree)) + r.count
   })
+  return count
+}
+
+function extractTypes(desc: string): Rule[] {
+  return desc.split(',').map(y => new Rule(y))
 }
 
 export default class Seven extends Command {
@@ -33,10 +44,10 @@ export default class Seven extends Command {
 
     const content = readFileSync(args.input, {encoding: 'UTF8'}).split('\n').filter(x => x)
     const tree = content.map(x => x.split(/ bags contain /)).reduce((acc, x) => {
-      extractTypes(x[1]).forEach(y => acc.has(y) ? acc.get(y).push(x[0]) : acc.set(y, [x[0]]))
+      acc.set(x[0], extractTypes(x[1]))
       return acc
     }, new Map())
-    const count = findOptions('shiny gold', tree, new Set())
-    this.log(`${count.size}`)
+    const count = countBags('shiny gold', tree)
+    this.log(`${count}`)
   }
 }
--- 2020/test/commands/seven.test.ts
+++ 2020/test/commands/seven.test.ts
@@ -4,7 +4,14 @@ describe('seven', () => {
   test
   .stdout()
   .command(['seven', 'resources/test-seven-a.txt'])
-  .it('runs seven', ctx => {
-    expect(ctx.stdout.trim()).to.equal('4')
+  .it('runs seven a', ctx => {
+    expect(ctx.stdout.trim()).to.equal('32')
+  })
+
+  test
+  .stdout()
+  .command(['seven', 'resources/test-seven-b.txt'])
+  .it('runs seven b', ctx => {
+    expect(ctx.stdout.trim()).to.equal('126')
   })
 })
