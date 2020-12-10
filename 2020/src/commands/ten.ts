364e56b1e2d5f7bed2fa7eea405d29002a795d19
Tim Mullender <t.j.mullender@gmail.com>
2020-12-10 23:07:27 +0000

    Day Ten - Part Two

--- 2020/src/commands/ten.ts
+++ 2020/src/commands/ten.ts
@@ -6,7 +6,7 @@ export default class Ten extends Command {
 
   static examples = [
     `$ aoc-2020 ten resources/test-ten-a.txt
-35
+8
 `,
   ]
 
@@ -18,15 +18,25 @@ export default class Ten extends Command {
     const {args} = this.parse(Ten)
 
     const content = readFileSync(args.input, {encoding: 'UTF8'}).split('\n').filter(x => x).map(x => parseInt(x, 10))
+    content.push(0)
     const adaptors = content.sort((a, b) => a - b)
-    const differences = [0, 0, 0, 0]
-    let last = 0
+    adaptors.push(adaptors[adaptors.length - 1] + 3)
+    const paths = [[adaptors[0], 1]]
     for (let i = 0; i < adaptors.length; i++) {
-      const difference = adaptors[i] - last
-      differences[difference] += 1
-      last = adaptors[i]
+      const next = adaptors.slice(i + 1, i + 4).filter(x => x <= adaptors[i] + 3)
+      const length = paths.length
+      let count = 0
+      for (let j = 0; j < length; j++) {
+        if (paths[j][0] === adaptors[i]) {
+          paths[j][0] = next[0]
+          count += paths[j][1]
+        }
+      }
+      if (next.length > 1) {
+        paths.push(...next.slice(1).map(x => [x, count]))
+      }
+      // console.log(paths)
     }
-    differences[3] += 1
-    this.log(`${differences[1] * differences[3]}`)
+    this.log(`${paths.reduce((acc, x) => acc + x[1], 0)}`)
   }
 }
--- 2020/test/commands/ten.test.ts
+++ 2020/test/commands/ten.test.ts
@@ -5,13 +5,20 @@ describe('ten', () => {
   .stdout()
   .command(['ten', 'resources/test-ten-a.txt'])
   .it('runs ten a', ctx => {
-    expect(ctx.stdout.trim()).to.equal('35')
+    expect(ctx.stdout.trim()).to.equal('8')
   })
 
   test
   .stdout()
   .command(['ten', 'resources/test-ten-b.txt'])
   .it('runs ten b', ctx => {
-    expect(ctx.stdout.trim()).to.equal('220')
+    expect(ctx.stdout.trim()).to.equal('19208')
+  })
+
+  test
+  .stdout()
+  .command(['ten', 'resources/test-ten-c.txt'])
+  .it('runs ten b', ctx => {
+    expect(ctx.stdout.trim()).to.equal('32')
   })
 })
