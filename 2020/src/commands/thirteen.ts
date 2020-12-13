4a77a4f1c265dfedb282bd3555a5924ee2b293f5
Tim Mullender <t.j.mullender@gmail.com>
2020-12-13 13:00:24 +0000

    Day Thirteen - Part Two

--- 2020/src/commands/thirteen.ts
+++ 2020/src/commands/thirteen.ts
@@ -1,12 +1,33 @@
 import {Command} from '@oclif/command'
 import {readFileSync} from 'fs'
+import {Big} from 'big.js'
+
+// stolen from https://en.wikipedia.org/wiki/Modular_exponentiation
+function modularExponentiation(a: number, b: number) {
+  if (b === 1) {
+    return 0
+  }
+  let base = a % b
+  let exponent = b - 2
+  let result = 1
+  while (exponent > 0) {
+    if (exponent % 2 === 1) {
+      result = (result * base) % b
+    }
+    exponent >>= 1
+    base = (base * base) % b
+  }
+  return result
+}
 
 export default class Thirteen extends Command {
   static description = 'A solver for Day Thirteen'
 
+  // Based on https://brilliant.org/wiki/chinese-remainder-theorem/
+
   static examples = [
     `$ aoc-2020 thirteen resources/test-thirteen-a.txt
-    295
+    1068781
 `,
   ]
 
@@ -18,19 +39,22 @@ export default class Thirteen extends Command {
     const {args} = this.parse(Thirteen)
 
     const content = readFileSync(args.input, {encoding: 'UTF8'}).split('\n')
-    const earliestDeparture = parseInt(content[0], 10)
-    const buses = content[1].split(',').filter(x => /\d+/.test(x)).map(x => parseInt(x, 10)).sort((a, b) => a - b)
-    let departureTime = earliestDeparture
-    while (departureTime <= earliestDeparture + buses[buses.length - 1]) {
-      buses.forEach(bus => {
-        const departures = departureTime / bus
-        if (departures === Math.floor(departures)) {
-          const wait = departureTime - earliestDeparture
-          this.log(`${wait * bus}`)
-          departureTime += earliestDeparture
-        }
-      })
-      departureTime++
+    const primes: number[] = []
+    const integers: number[] = []
+    content[1].split(',').forEach((x, i) => {
+      if (x !== 'x') {
+        const bus = parseInt(x, 10)
+        primes.push(bus)
+        integers.push(((10 * bus) - i) % bus)
+      }
+    })
+    const N = primes.reduce((acc, x) => acc * x, 1)
+    let departureTime = new Big(0)
+    for (let i = 0; i < primes.length; i++) {
+      const prime = primes[i]
+      const z = Math.floor(N / primes[i])
+      departureTime = departureTime.add(new Big(integers[i] * z).mul(modularExponentiation(z, prime)))
     }
+    this.log(`${departureTime.mod(N)}`)
   }
 }
--- 2020/test/commands/thirteen.test.ts
+++ 2020/test/commands/thirteen.test.ts
@@ -4,7 +4,42 @@ describe('thirteen', () => {
   test
   .stdout()
   .command(['thirteen', 'resources/test-thirteen-a.txt'])
-  .it('runs thirteen', ctx => {
-    expect(ctx.stdout.trim()).to.equal('295')
+  .it('runs thirteen a', ctx => {
+    expect(ctx.stdout.trim()).to.equal('1068781')
+  })
+
+  test
+  .stdout()
+  .command(['thirteen', 'resources/test-thirteen-b.txt'])
+  .it('runs thirteen b', ctx => {
+    expect(ctx.stdout.trim()).to.equal('3417')
+  })
+
+  test
+  .stdout()
+  .command(['thirteen', 'resources/test-thirteen-c.txt'])
+  .it('runs thirteen c', ctx => {
+    expect(ctx.stdout.trim()).to.equal('754018')
+  })
+
+  test
+  .stdout()
+  .command(['thirteen', 'resources/test-thirteen-d.txt'])
+  .it('runs thirteen d', ctx => {
+    expect(ctx.stdout.trim()).to.equal('779210')
+  })
+
+  test
+  .stdout()
+  .command(['thirteen', 'resources/test-thirteen-e.txt'])
+  .it('runs thirteen e', ctx => {
+    expect(ctx.stdout.trim()).to.equal('1261476')
+  })
+
+  test
+  .stdout()
+  .command(['thirteen', 'resources/test-thirteen-f.txt'])
+  .it('runs thirteen f', ctx => {
+    expect(ctx.stdout.trim()).to.equal('1202161486')
   })
 })
